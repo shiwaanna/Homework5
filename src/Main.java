92d7b3e3fe6cd328c7f2191800fb8c3e41c08946
anna <shiwa.ann@gmail.com>
2023-05-16 21:39:09 +0400

    Исправлена 34 строка

--- src/Main.java
+++ src/Main.java
@@ -32,7 +32,7 @@ public class Main {
     public static void task3() {
         System.out.println("Задача 3");
         int year = 2021;
-        if (year%4 == 0 || year%400 == 0 && year%100 != 0) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
             System.out.println(year + " год является високосным");
         } else {
             System.out.println(year + " год не является високосным");
